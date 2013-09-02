ddeb1fa2474ef3a41f0fafd3c27d2c5268df71e4
Seigo Uchida <spesnova@gmail.com>
2013-09-02 22:02:39 +0900

    Initial commit. Prepare cookbook template.

---
+++ README.md
@@ -0,0 +1,13 @@
+# spesnova-hubot cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Seigo Uchida (<spesnova@gmail.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'spesnova-hubot'
+maintainer       'Seigo Uchida'
+maintainer_email 'spesnova@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures spesnova-hubot'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
